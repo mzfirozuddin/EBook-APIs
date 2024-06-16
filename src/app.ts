2c315b133322e7145e8967235825636442a7554c
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-16 23:07:15 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
 
 const app = express();
+
+// Global Error Handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+
+    return res.status(statusCode).json({
+        message: err.message,
+        errorStack: config.env === "development" ? err.stack : "",
+    });
+});
 
 export default app;

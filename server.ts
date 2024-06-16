9f86ec482e0ce9230ed309708b2c1f9d383e0433
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-16 15:37:57 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome to eBook APIs");
+import app from "./src/app";
+
+const startServer = () => {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}...`);
+    });
+};
+
+startServer();

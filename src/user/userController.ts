495eef4866e7fc3e6d8dacefcfb7c34d67f389b4
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-17 23:51:42 +0530

    hashed password, create a new user and sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
+import bcrypt from "bcrypt";
+import { User } from "./userModel";
+import createHttpError from "http-errors";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     //: Validation
@@ -8,9 +13,34 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return res.status(400).json({ error: result.array() });
     }
 
-    //: Process
+    const { name, email, password } = req.body;
+
+    //: DB Call (Check user is already present or not in DB)
+    const user = await User.findOne({ email });
+    if (user) {
+        const error = createHttpError(
+            400,
+            "User already exists with this email!"
+        );
+        return next(error);
+    }
+
+    //: Password Hash
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //: Store user in DB
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    //: JWT token generation
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+        expiresIn: "7d",
+    });
     //: Response
-    res.json({ message: "User created!" });
+    res.json({ id: newUser._id, accessToken: token });
 };
 
 export { createUser };
--- src/user/userModel.ts
+++ src/user/userModel.ts
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema<IUser>(
             type: String,
             required: true,
             unique: true,
+            index: true,
         },
         password: {
             type: String,

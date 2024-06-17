7e71244b4a22270dd35ededf8e071d31c93ded3c
mzfirozuddin <mz.firozuddin@gmail.com>
2024-06-17 23:04:06 +0530

    create user model

---
+++ src/user/userModel.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { IUser } from "./userTypes";
+
+const userSchema = new mongoose.Schema<IUser>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>("User", userSchema);
